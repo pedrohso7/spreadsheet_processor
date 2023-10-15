b99d273c5b405331088f00d55b1d1ee6200c5017
Pedro Oliveira <pholiveira1998@hotmail.com>
2023-10-15 13:46:58 -0400

    refactor: refactor processing data approach.

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,50 +1,43 @@
 import { Injectable } from '@nestjs/common';
 import * as xlsx from 'xlsx';
+import { Tag } from './entities/tag.entity';
 
 @Injectable()
 export class AppService {
-  async handleProcessFileFromQueue(fileBuffer: string): Promise<void> {
-    try {
-      const fileData = Buffer.from(fileBuffer, 'base64');
-  
-      const workbook = xlsx.read(fileData, { type: 'buffer' });
-      const worksheet = workbook.Sheets[workbook.SheetNames[0]];
-  
-      const rows = [];
-  
-      for (const cell in worksheet) {
-        if (!cell.startsWith('!')) {
-          const cellData = worksheet[cell];
-  
-          if (cellData.w === 'MaisEnvios Testes') {
-            continue;
-          }
-  
-          const cellIndex = parseInt(cell.substring(1));
-  
-          if (cellIndex < 4) {
-            continue;
-          }
-  
-          const row = {
-            tag: worksheet['A' + cell.substring(1)].w,
-            name: worksheet['B' + cell.substring(1)].w,
-            status: parseInt(worksheet['C' + cell.substring(1)].w),
-            source: worksheet['D' + cell.substring(1)].w,
-            price: parseFloat(worksheet['E' + cell.substring(1)].w),
-          };
-  
-          rows.push(row);
-        }
-      }
-  
-      const filteredArray = rows.filter(
-        (obj, index, self) => index === self.findIndex((o) => o.tag === obj.tag),
-      );
-  
-      // await this.repository.insert(filteredArray);
-    } catch (error) {
-      console.log(error);
+  async handleProcessFileFromQueue(fileBuffer: string) {
+    const fileDataBuffer = Buffer.from(fileBuffer, 'base64');
+
+    const workbook = xlsx.read(fileDataBuffer, { type: 'buffer' });
+    const worksheet = workbook.Sheets[workbook.SheetNames[0]];
+    
+    const rows = [];
+
+    for (const cell in worksheet) {
+      if(cell[0] != 'A') continue;
+      const cellIndex = parseInt(cell.substring(1));
+      if(cellIndex < 4) continue;
+      
+      const row = new Tag({
+        tag: worksheet['A' + cell.substring(1)].w,
+        name: worksheet['B' + cell.substring(1)].w,
+        status: parseInt(worksheet['C' + cell.substring(1)].w),
+        source: worksheet['D' + cell.substring(1)].w,
+        price: parseFloat(worksheet['E' + cell.substring(1)].w),
+      });
+
+      rows.push(row);
     }
+
+    const extractedTags: Tag[] = rows.map(
+      (row) => new Tag({
+        tag: row.tag,
+        name: row.name,
+        status: row.status,
+        source: row.source,
+        price: row.price,
+      }),
+    );
+    
+    return extractedTags;
   }
 }
